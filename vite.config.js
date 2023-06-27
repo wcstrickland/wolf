acd932326c6c8fc4fc3ee1ee78803734d88fc73d
bill <wcstrickland1@gmail.com>
2023-06-27 14:30:06 -0500

    pwa

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), VitePWA()],
   base: 'https://wcstrickland.github.io/wolf/',
   build:{
     outDir: './docs'

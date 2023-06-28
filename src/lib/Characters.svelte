90b482f10c4b5747a1c9a5f16f1c124d56d764c0
bill <wcstrickland1@gmail.com>
2023-06-28 16:58:30 -0500

    added classes and maybe safari icon

--- src/lib/Characters.svelte
+++ src/lib/Characters.svelte
@@ -9,13 +9,40 @@
     <button on:click={() => setCharacter(characters["rogue"])}>The Rogue</button
     > Agile, Charismatic, and Sneaky
   </li>
-  <li class="char-list"><button on:click={()=> setCharacter(characters["brawler"])}>The Brawler</button> Strong, Tough, and Forceful</li>
+  <li class="char-list">
+    <button on:click={() => setCharacter(characters["brawler"])}
+      >The Brawler</button
+    > Strong, Tough, and Forceful
+  </li>
+  <li class="char-list">
+    <button on:click={() => setCharacter(characters["knight"])}
+      >The Knight</button
+    > A natural fighter with noble bearing
+  </li>
+  <li class="char-list">
+    <button on:click={() => setCharacter(characters["face"])}>The Face</button>A
+    silver tongue made flesh
+  </li>
+  <li class="char-list">
+    <button on:click={() => setCharacter(characters["investigator"])}
+      >The Investigator</button
+    >Capable and Curious
+  </li>
+  <li class="char-list">
+    <button on:click={() => setCharacter(characters["healer"])}
+      >The Healer</button
+    >Focused on Medicine and being Healthy
+  </li>
+  <li class="char-list">
+    <button on:click={() => setCharacter(characters["scientist"])}
+      >The Scientist</button
+    >The titular Academic
+  </li>
 </ul>
 <HomeButton />
 
 <style>
-
-    .char-list{
-        margin-bottom: .5em;
-    }
+  .char-list {
+    margin-bottom: 0.5em;
+  }
 </style>
